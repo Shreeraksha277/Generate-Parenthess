bb1ae31393f4dd61c739dc4209316b9313e66365
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-06-14 18:00:37 +0530

    Update sol.java

--- sol.java
+++ sol.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public List<String> generateParenthesis(int n) {
+     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
         backtrack(result, "", 0, 0, n);
         return result;
